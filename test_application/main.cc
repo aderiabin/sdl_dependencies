e5f159236c56b2da016a0acdfb930c08be8dadf9
Alex Kutsan <akutsan@luxoft.com>
2018-02-28 01:11:56 +0200

    Add json test

--- test_application/main.cc
+++ test_application/main.cc
@@ -1,5 +1,24 @@
+#include <iostream>
 #include <json/json.h>
 #include <log4cxx/logger.h>
+
+int jsontest() {
+	Json::Value root;
+	root["key"] = "value";
+	std::string root_str = root.toStyledString();
+    
+	Json::Reader reader;
+	Json::Value parsed;
+	reader.parse(root_str, parsed);
+
+	std::string value = root["key"];
+	assert (value == "value");
+	if ("value" != value) {
+		std::cout << "Json get key failed" << std::endl;
+		return false;
+	}
+	return true;
+}
 
 int main() {
 	return 0;
