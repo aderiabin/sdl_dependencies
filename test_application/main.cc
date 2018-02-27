fabc9e64cb2def33c5b55b4b9f4ca4a52159077b
Alex Kutsan <akutsan@luxoft.com>
2018-02-28 01:48:24 +0200

    CHeck app work for linux

--- test_application/main.cc
+++ test_application/main.cc
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <cassert>
+#include <fstream>
 #include <json/json.h>
 #include <log4cxx/logger.h>
+#include <log4cxx/propertyconfigurator.h>
+#include <log4cxx/spi/loggingevent.h>
 
-int jsontest() {
+bool jsontest() {
 	Json::Value root;
 	root["key"] = "value";
 	std::string root_str = root.toStyledString();
@@ -11,15 +15,37 @@ int jsontest() {
 	Json::Value parsed;
 	reader.parse(root_str, parsed);
 
-	std::string value = root["key"];
-	assert (value == "value");
+	std::string value = root["key"].asString();
 	if ("value" != value) {
-		std::cout << "Json get key failed" << std::endl;
+		std::cout << "Json test failed" << std::endl;
 		return false;
 	}
 	return true;
 }
 
+bool logger_test() {
+	log4cxx::PropertyConfigurator::configure("log4cxx.properties");
+    log4cxx::LoggerPtr loggerMyMain(log4cxx::Logger::getLogger( "MainLogger"));
+	std::string message = "DebugMessage";
+	LOG4CXX_DEBUG(loggerMyMain, message);
+	std::string log_content;
+	std::ifstream infile;
+	infile.open ("logfile.log");
+	infile >> log_content;
+	if (log_content != message) {
+		std::cout << "Logger test failed" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+
 int main() {
+	if(jsontest()) {
+		std::cout << "Json test passed" << std::endl;
+	}
+	if(logger_test()) {
+		std::cout << "Logger test passed" << std::endl;
+	}
 	return 0;
 }
