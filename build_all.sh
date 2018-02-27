0433d6ceef21511571b877f5df32b993988bf794
Alex <akutsan@luxoft.com>
2018-02-27 22:36:48 +0000

    Remove redundant files

--- build_all.sh
+++
@@ -1,8 +0,0 @@
-./build_json.sh &&\
-echo "======= JSON successfuly built! =======" &&\
-./build_apr.sh &&\
-echo "======= APR successfuly built! =======" &&\
-./build_apr-util.sh &&\
-echo "======= APR UTIL successfuly built! =======" &&\
-./build_logger.sh
-echo "======= LOGGER successfuly built! ======="
